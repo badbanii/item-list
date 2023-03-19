2342759d9686a9e31eaf1456d095f68b1da18727
Tulitu Vlad <tulituvlad@gmail.com>
2023-03-19 17:49:21 +0200

    Initial Commit

---
+++ Item List/Item_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Item_ListApp.swift
+//  Item List
+//
+//  Created by Vlad on 3/19/23.
+//
+
+import SwiftUI
+
+@main
+struct Item_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
